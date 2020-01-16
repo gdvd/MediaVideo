caf4e9b65e42e193778a5704c5e594c3a0625346
gillesdavid <gdvd@me.com>
2020-01-16 13:01:17 +0100

    Basket finish

--- src/main/java/fr/gdvd/media_manager/sec/SecurityParams.java
+++ src/main/java/fr/gdvd/media_manager/sec/SecurityParams.java
@@ -4,6 +4,6 @@ public interface SecurityParams {
 
     public static final String JWT_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String PRIVATE_SECRET = "PassWord"; // Your PassWord
+    public static final String PRIVATE_SECRET = "writeYourOwnPassword";
     public static final long EXPIRATION = 1000 * 60 * 60 * 36; // millisec*1000 * s*60 * mn*60 * h*36 = 36h
 }
