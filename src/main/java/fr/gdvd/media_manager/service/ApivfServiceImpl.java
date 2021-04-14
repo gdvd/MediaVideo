49a7638d3943929d70cdb0d86cdfb3fd0de13ae4
gdvd <gdvd@me.com>
2021-04-14 16:04:09 +0200

    RSS V1.1.2

--- src/main/java/fr/gdvd/media_manager/service/ApivfServiceImpl.java
+++ src/main/java/fr/gdvd/media_manager/service/ApivfServiceImpl.java
@@ -83,37 +83,32 @@ public class ApivfServiceImpl implements ApivfService {
         List<Item> li = new ArrayList<>();
         for (Map.Entry<String, List<VideoFilm>> e : mlsul.entrySet()) {
             String login = e.getKey();
-            Item i = new Item();
             String db = "DB";
-            if (login.equals(db)) {
-                for (VideoFilm vf : e.getValue()) {
+            for (VideoFilm vf : e.getValue()) {
+                Item i = new Item();
 
+                if (login.equals(db)) {
                     i.setTitle(vf.getVideoTitles().get(0).getTitle() + " : " +
                             ((float) vf.getScoreOnHundred() / 10) + "/10 (" + db + ")");
-                    i.setDescription(vf.getVideoPosters().get(0).getUlrImg());
                     i.setPubDate(vf.getDateModifFilm().toString());
-                    i.setLink(URL_SITE + "/videoid?title=" +
-                            Base64.getUrlEncoder().encodeToString(vf.getIdVideo().getBytes()) +
-                            "&sortValue=2&sortOrder=0&sortUser=0&charsreadySel=1&keywordTitleIsSel=1");
                     i.setComment("");
-                    li.add(i);
-                }
-            } else {
-                for (VideoFilm vf : e.getValue()) {
+                } else {
                     List<VideoUserScore> lvu = vf.getVideoUserScores().stream()
                             .filter(vus -> vus.getMyUser().getLogin().equals(login))
                             .collect(Collectors.toList());
                     VideoUserScore vu = lvu.get(0);
                     i.setTitle(vf.getVideoTitles().get(0).getTitle() + " : " +
                             ((float) vu.getNoteOnHundred() / 10) + "/10 (" + login + ")");
-                    i.setDescription(vf.getVideoPosters().get(0).getUlrImg());
                     i.setPubDate(vu.getDateModifScoreUser().toString());
-                    i.setLink(URL_SITE + "/videoid?title=" +
-                            Base64.getUrlEncoder().encodeToString(vf.getIdVideo().getBytes()) +
-                            "&sortValue=2&sortOrder=0&sortUser=0&charsreadySel=1&keywordTitleIsSel=1");
                     i.setComment(vu.getCommentScoreUser() != null ? vu.getCommentScoreUser().getComment() : "");
-                    li.add(i);
                 }
+
+
+                i.setDescription(vf.getVideoTitles().get(0).getTitle());
+                i.setLink(URL_SITE + "/videoid?title=" +
+                        Base64.getUrlEncoder().encodeToString(vf.getIdVideo().getBytes()) +
+                        "&sortValue=2&sortOrder=0&sortUser=0&charsreadySel=1&keywordTitleIsSel=1");
+                li.add(i);
             }
         }
         return li;
@@ -122,12 +117,12 @@ public class ApivfServiceImpl implements ApivfService {
     private Map<String, List<VideoFilm>> lastScoreBylogin(int quatity, String[] ids) {
         Map<String, List<VideoFilm>> mlsul = new HashMap();
         for (String usr : ids) {
-            if (usr.equals("0")) {
+            Long idUsr = Long.parseLong(usr);
+            if(idUsr==0){
                 Pageable pageable = PageRequest.of(0, quatity);
                 List<VideoFilm> l = videoFilmRepository.findVFByLast(pageable);
                 mlsul.put("DB", l);
-            } else {
-                Long idUsr = Long.parseLong(usr);
+            }else{
                 String username = getLogin(idUsr).orElse(null);
                 if (username != null) {
                     Pageable pageable = PageRequest.of(0, quatity);
@@ -136,6 +131,7 @@ public class ApivfServiceImpl implements ApivfService {
                     mlsul.put(username, l);
                 }
             }
+
         }
         return mlsul;
     }
@@ -143,31 +139,5 @@ public class ApivfServiceImpl implements ApivfService {
     private Optional<String> getLogin(Long idUsr) {
         return myUserRepository.findLoginByIdUser(idUsr);
     }
-
-    /*private List<ScoreUserLight> convertListTupleInListSUL(List<Tuple> lt) {
-        List<ScoreUserLight> lsul = new ArrayList<>();
-        if (lt.size() > 0) {
-            for (Tuple t : lt) {
-                ScoreUserLight sul = new ScoreUserLight(
-                        (String) t.toArray()[0],
-                        getFirstTitle((String) t.toArray()[0]),
-                        (int) t.toArray()[1],
-                        (String) t.toArray()[2],
-                        (Timestamp) t.toArray()[3]
-                );
-                sul.setTitle(videoTitleRepository.findTitlesByIdvideo(sul.getIdtt()).get(0));
-                sul.setIdtt(URL_SITE + "/videoid?title=" +
-                        Base64.getEncoder().encodeToString((sul.getIdtt()).getBytes()) +
-                        "&sortValue=2&sortOrder=0&sortUser=0&charsreadySel=1&keywordTitleIsSel=1");
-                lsul.add(sul);
-            }
-        }
-        return lsul;
-    }
-
-    private String getFirstTitle(String idtt) {
-        List<String> lt = videoTitleRepository.findTitlesByIdvideo(idtt);
-        return lt.get(0);
-    }*/
 
 }
